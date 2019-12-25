b5d04337d17fe4eb369f2edcec1f6251f99a34e5
e <mrxianx@163.com>
2019-12-25 21:33:25 +0800

    1

--- day01/const/main.go
+++ day01/const/main.go
@@ -29,18 +29,71 @@ const (
 	a4        //4
 )
 
+// 插入
+const (
+	b1 = iota // 0
+	b2 = 100  //100
+	b3 = iota //2(const 中每新增一行自增加一)
+	b4        //3
+)
+
+const (
+	c1 = iota // 0
+	c2 = 100  //100
+	c3 = 101  //101
+	c4 = iota //3
+	c5        //4
+)
+
+// 多个常量定义在一行
+const (
+	e1, e2 = iota + 1, iota + 2 //e1=1,e2=2
+	e3, e4 = iota + 1, iota + 2 //e3=2,e4=3
+)
+
+// 定义数组集
+const (
+	_  = iota
+	KB = 1 << (iota * 10)
+	MB = 1 << (iota * 10)
+	GB = 1 << (iota * 10)
+	TB = 1 << (iota * 10)
+)
+
 func main() {
 	// a = 5 不能覆盖常量的值
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(e)
-	fmt.Println(f)
-	fmt.Println(g)
 
-	fmt.Println(a1)
-	fmt.Println(a2)
-	fmt.Println(a3)
-	fmt.Println(a4)
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+	// fmt.Println(d)
+	// fmt.Println(e)
+	// fmt.Println(f)
+	// fmt.Println(g)
+
+	// fmt.Println(a1)
+	// fmt.Println(a2)
+	// fmt.Println(a3)
+	// fmt.Println(a4)
+
+	// fmt.Println(b1)
+	// fmt.Println(b2)
+	// fmt.Println(b3)
+	// fmt.Println(b4)
+
+	// fmt.Println(c1)
+	// fmt.Println(c2)
+	// fmt.Println(c3)
+	// fmt.Println(c4)
+	// fmt.Println(c5)
+
+	// fmt.Println(e1)
+	// fmt.Println(e2)
+	// fmt.Println(e3)
+	// fmt.Println(e4)
+
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
 }
